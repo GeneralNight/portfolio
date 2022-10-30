941de7e075f0ea925773a19cc138190d31d91648
Guilherme Tinen <gfmi0911@hotmail.com>
2022-10-30 00:37:42 -0300

    ga and gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,23 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+      {hid: 'keywords',name: 'keywords',content: `Ecoder, Guilherme Tinen Ortolani, Guilherme Tinen, Desenvolvdor Frontend, Desenvolvedor Backend, Developer, Frontend Developer, VueJs`},
+      {hid: 'author',name: 'author',content: `Guilherme Tinen Ortolani`},
+      {hid: 'description',name: 'description',content: `Each one has the power to change world, here is my way... developing and allowing companies achieve your goals through technology. `},
+
+      {hid: 'og:locale',name: 'og:locale',content: `pt_br`},
+      {hid: 'og:site_name',name: 'og:site_name',content: `Guilherme Tinen Ortolani`},
+      {hid: 'og:title',name: 'og:title',content: `Portfolio`},
+      {hid: 'og:type',name: 'og:type',content: `website`},
+      {hid: 'og:image',name: 'og:image',content: `https://ecoder.com.br/cover.png`},
+      {hid: 'og:url',name: 'og:url',content: `https://ecoder.com.br/`},
+      {hid: 'og:description',name: 'og:description',content: `Each one has the power to change world, here is my way... developing and allowing companies achieve your goals through technology. `},
+      {hid: 'og:image:width',name: 'og:image:width',content: `1920`},
+      {hid: 'og:image:height',name: 'og:image:height',content: `320`},
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.jpg' }
     ],
     script: [
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -47,6 +59,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/google-gtag',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -66,5 +80,28 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  googleAnalytics: {
+    id: 'G-T1VB956MVJ'
+  },
+
+  'google-gtag': {
+    // id: 'UA-240578233-1', // required
+    id: 'G-T1VB956MVJ', // required
+    config:{
+      // this are the config options for `gtag
+      // check out official docs: https://developers.google.com/analytics/devguides/collection/gtagjs/
+      anonymize_ip: true, // anonymize IP 
+      send_page_view: true, // might be necessary to avoid duplicated page track on page reload
+      linker:{
+        domains:['ecoder.com.br']
+      }
+    },
+    debug: true, // enable to track in dev mode
+    disableAutoPageTrack: true, // disable if you don't want to track each page route with router.afterEach(...)
+    // optional you can add more configuration like [AdWords](https://developers.google.com/adwords-remarketing-tag/#configuring_the_global_site_tag_for_multiple_accounts)
+    additionalAccounts:[]
+  },
+
 }
